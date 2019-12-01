9a9acbeda3d0794e4153ceba80eae7484244f46c
Desktop | Tuxi Metal <github@lgdweb.fr>
2019-12-01 11:14:04 +0100

    Update App.js file; add basic routes and show the navigation Menu

--- src/App.js
+++ src/App.js
@@ -1,7 +1,28 @@
 import React from 'react'
+import { BrowserRouter as Router, Route, Redirect, Switch } from 'react-router-dom'
+
+import Home from './main/pages/Home'
+import Auth from './users/pages/Auth'
+import { Profiles, Profile, AddEducation, AddExperience, EditProfile } from './profiles/pages'
+import { Posts, Post } from './posts/pages'
+import Menu from './shared/components/Navigation/Menu'
 
 const App = () => (
-  <h1>Dev Connector</h1>
+  <Router>
+    <Menu />
+    <Switch>
+      <Route path='/' exact component={Home} />
+      <Route path='/auth' component={Auth} />
+      <Route path='/profiles' exact component={Profiles} />
+      <Route path='/profiles/edit' component={EditProfile} />
+      <Route path='/profiles/education' component={AddEducation} />
+      <Route path='/profiles/experience' component={AddExperience} />
+      <Route path='/profiles/:profileId' component={Profile} />
+      <Route path='/posts' exact component={Posts} />
+      <Route path='/posts/:postId' component={Post} />
+      <Redirect to='/' />
+    </Switch>
+  </Router>
 )
 
 export default App
